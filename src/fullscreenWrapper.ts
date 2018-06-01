8c200df9b87b81a75b422a5153e30236d1f55ecb
Christoph Hummel <christoph.hummel@virtual-identity.com>
2018-06-01 13:22:21 +0200

    add calc

--- src/fullscreenWrapper.ts
+++ src/fullscreenWrapper.ts
@@ -9,8 +9,56 @@ export default function (option: FullscreenWrapperObject) {
     }
     let element = option.element;
 
+
+    let getOrientation = () => {
+        if (window.innerWidth >= window.innerHeight) {
+            return "LANDSCAPE"
+        }
+        return "PORTRAIT"
+    };
+
+    let setCurrentDemoHTML = () => {
+        document.querySelector('.innerWidth').innerHTML = window.innerWidth.toString();
+        document.querySelector('.innerHeight').innerHTML = window.innerHeight.toString();
+        document.querySelector('.screenWidth').innerHTML = getRealScreenSize().width.toString();
+        document.querySelector('.screenHeight').innerHTML = getRealScreenSize().height.toString();
+    };
+
+    let calcWrapperScale = () => {
+        let elementHeight = element.clientHeight;
+        let elementWidth = element.clientWidth;
+        let minScale = Math.min( getRealScreenSize().height / elementHeight, getRealScreenSize().width / elementWidth);
+        element.style.transform = 'scale(' + parseFloat((minScale).toFixed(3)) + ')';
+    };
+
+    let getRealScreenSize = () => {
+       let width;
+       let height;
+        // if (Platform.isDesktopDevice) {
+        //     return f.getInnerScreenSize()
+        // }
+        if (getOrientation() === "PORTRAIT") {
+            width = Math.min(window.screen.width, window.screen.height);
+            height = Math.max(window.screen.width, window.screen.height)
+        } else {
+            if (getOrientation() === "LANDSCAPE") {
+                width = Math.max(window.screen.width, window.screen.height);
+                height = Math.min(window.screen.width, window.screen.height)
+            }
+        }
+        return {
+            width: width,
+            height: height
+        }
+    };
+
     let init = () => {
-        console.log(element);
+        setCurrentDemoHTML();
+        calcWrapperScale();
+        window.addEventListener('resize', () => {
+            setCurrentDemoHTML();
+            calcWrapperScale();
+        });
     };
     init();
 }
